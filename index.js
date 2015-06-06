04ea8ef0400e3193349c40455be5ba1f10e86291
Scott Donnelly <scott@donnel.ly>
2015-06-06 19:12:01 +0100

    reduced dependancy on lodash to target individual lodash functions.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 var path = require('path'),
-    _    = require('lodash');
+    assign = require('lodash.assign'),
+    merge = require('lodash.merge'),
+    reduce = require('lodash.reduce');
 
 var envs = (process.env.NODE_ENV || 'dev').split('-');
 
@@ -10,9 +12,7 @@ module.exports = function(dir, options) {
 	var defaults = {
 		base: 'all'
 	};
-	var opt      = _.extend(defaults, options);
-
-	var test = process.env.NODE_ENV == 'test-configLayered';
+	var opt = assign(defaults, options);
 
 	var baseConfig = {};
 	var basePath   = path.join(__dirname, test ? 'test' : '..', test ? '' : '..', dir, opt.base);
@@ -22,14 +22,14 @@ module.exports = function(dir, options) {
 		console.log('Missing config file: \'' + basePath + '.js\'');
 	}
 
-	return _.reduce(envs,
+	return reduce(envs,
 		function(acc, env, idx, envs) {
 
 			var nextLayer = envs.slice(0, idx + 1).join('-');
 
 			var confPath = path.join(__dirname, test ? 'test' : '..', test ? '' : '..', dir, nextLayer);
 			try {
-				acc = _.merge(acc, require(confPath));
+				acc = merge(acc, require(layerPath));
 			} catch (e) {
 				console.log('Missing config file ' + confPath);
 			}
